ad6b51c905bd8f22398bfac377ec25afa21ea688
olakepro <99777517+olakepro@users.noreply.github.com>
2023-04-25 22:28:14 -0500

    Update main.js

--- main.js
+++ main.js
@@ -49,7 +49,7 @@ function date()
     let day = today.getUTCDate();
     let month = months[today.getMonth()];
     getId("month").innerHTML = month.toUpperCase();
-    getId("monthDay").innerHTML = ("🌐" + day + "🌐").slice(-7);
+    getId("monthDay").innerHTML = ("🗓️" + day).slice(-7);
     getId("day").innerHTML = wordDay(today.getDay())
 }
 
